42b5eab705007a344a0f640117d85daae260189e
Nitish Singla <nitish.singla@hmcts.net>
2021-01-23 14:30:42 +0000

    Fixing the PR build.

--- src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/test/SampleTest.java
+++ src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/test/SampleTest.java
@@ -3,14 +3,10 @@ package uk.gov.hmcts.reform.orgrolemapping.domain.test;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Date;
-
 public class SampleTest {
 
     @Test
     public void dummyTest() {
-        Date d1 = new Date();
-        Date d2 = new Date();
-        Assert.assertNotEquals(d1.getTime(), d2.getTime());
+        Assert.assertEquals(1, 1);
     }
 }
