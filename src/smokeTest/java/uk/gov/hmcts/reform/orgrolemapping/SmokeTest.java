ab8852b1c558e5fe2b03eee734154c5c726f6a27
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2021-01-25 14:16:26 +0000

    added the smoke testing again

---
+++ src/smokeTest/java/uk/gov/hmcts/reform/orgrolemapping/SmokeTest.java
@@ -0,0 +1,41 @@
+package uk.gov.hmcts.reform.orgrolemapping;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.WithTag;
+import net.thucydides.core.annotations.WithTags;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RunWith(SpringIntegrationSerenityRunner.class)
+@WithTags({@WithTag("testType:Smoke")})
+public class SmokeTest {
+
+    // use this when testing locally - replace the below content with this line
+    private final String targetInstance =
+            StringUtils.defaultIfBlank(
+                    System.getenv("TEST_URL"),
+                    "http://localhost:5010"
+            );
+
+    @Test
+    public void test_should_prove_app_is_running_and_healthy() {
+        RestAssured.baseURI = targetInstance;
+        RestAssured.useRelaxedHTTPSValidation();
+
+        Response response = SerenityRest
+                .given()
+                .relaxedHTTPSValidation()
+                .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                .get("/health")
+                .andReturn();
+        Assert.assertEquals(200, response.statusCode());
+    }
+}
